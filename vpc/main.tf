d14de8f8455644085a1a0e5f35fcb2dbd3d73fcd
choisungwook <kgg1959@naver.com>
2022-05-01 16:01:59 +0900

    route table 추가 #3

--- vpc/main.tf
+++ vpc/main.tf
@@ -29,3 +29,31 @@ resource "aws_subnet" "private" {
     Name = "${var.name}-private-${each.key}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  
+  tags = {
+    Name = "${var.name}-public"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = var.public_subnet
+
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public.id
+}
